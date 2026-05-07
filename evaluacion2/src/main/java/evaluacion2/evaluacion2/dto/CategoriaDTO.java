ba99dcfee7e5b8d559c64a10cc71fdf05e2b12ee
mat-di <mata.diazs@duocuc.cl>
2026-05-07 15:55:06 -0400

    Update CategoriaDTO.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/CategoriaDTO.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/CategoriaDTO.java
@@ -1,9 +1,12 @@
 package evaluacion2.evaluacion2.dto;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class CategoriaDTO {
     private Integer id;
     private String nombre;
+    private List<String> tituloLibros;
 }
