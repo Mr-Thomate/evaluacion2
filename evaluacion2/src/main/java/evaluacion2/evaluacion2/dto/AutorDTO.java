5a39fea34fe06f47bb8f51e86e468c1781b5f7fa
mat-di <mata.diazs@duocuc.cl>
2026-05-07 15:53:50 -0400

    Update AutorDTO.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/AutorDTO.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/AutorDTO.java
@@ -1,9 +1,12 @@
 package evaluacion2.evaluacion2.dto;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class AutorDTO {
     private Integer id;
     private String nombre;
+    private List<String> tituloLibros;
 }
