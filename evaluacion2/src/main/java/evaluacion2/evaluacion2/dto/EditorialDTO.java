37bba67e7f26e16cba9451d747957e09efd98036
mat-di <mata.diazs@duocuc.cl>
2026-05-07 15:56:46 -0400

    Update EditorialDTO.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/EditorialDTO.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/EditorialDTO.java
@@ -1,9 +1,12 @@
 package evaluacion2.evaluacion2.dto;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class EditorialDTO {
     private Integer idEditorial;
     private String nombre;
+    private List<String> tituloLibros; //mostrar el titulo de los libros que tiene la editorial
 }
