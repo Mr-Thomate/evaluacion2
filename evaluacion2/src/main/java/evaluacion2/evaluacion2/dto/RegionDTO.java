a576006c8e88a537007475dfccd1599a65598a1f
mat-di <mata.diazs@duocuc.cl>
2026-05-07 15:58:15 -0400

    Update RegionDTO.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/RegionDTO.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/dto/RegionDTO.java
@@ -1,9 +1,12 @@
 package evaluacion2.evaluacion2.dto;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class RegionDTO {
     private Integer idRegion;
     private String nombreRegion;
+    private List<String> nombreComunas; //mostrar el nombre de las comunas que se encuentran en la region
 }
