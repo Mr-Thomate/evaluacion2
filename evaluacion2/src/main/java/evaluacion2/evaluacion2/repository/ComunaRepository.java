b8e215a5733995adafbc78be6cb370e0a5e23fad
Thomas <th.mery@duocuc.cl>
2026-05-09 20:45:52 -0400

    Añadido de querys en los repositorys
    Se han añadido querys en todos los repositorys menos en autor, categoria y editorial, para aumentar las posibilidades de navegacion desde un mismo tipo de repository.

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/ComunaRepository.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/ComunaRepository.java
@@ -3,12 +3,19 @@ package evaluacion2.evaluacion2.repository;
 import org.springframework.stereotype.Repository;
 
 import evaluacion2.evaluacion2.model.Comuna;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 
 
 @Repository
 public interface ComunaRepository extends JpaRepository<Comuna, Integer>{
-
+    // Busqueda de comuna mediante region asociada
+    @Query("SELECT c FROM Comuna c JOIN c.region r WHERE r.id = :idRegion")
+    List<Comuna> findByIdRegion(@Param("idRegion") Integer idRegion);
 }
--- evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/PrestamoRepository.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/PrestamoRepository.java
@@ -1,14 +1,28 @@
 package evaluacion2.evaluacion2.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import evaluacion2.evaluacion2.model.Prestamo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 
 
 @Repository
 public interface PrestamoRepository extends JpaRepository<Prestamo, Integer>{
+    // Busqueda de prestamo por biblioteca asociada
+    @Query("SELECT p FROM Prestamo p JOIN p.biblioteca b WHERE b.id = :idBiblioteca")
+    List<Prestamo> findByIdBiblioteca(@Param("idBiblioteca") Integer idBiblioteca);
+    
+    // Busqueda de prestamo por cliente asociado
+    @Query("SELECT p FROM Prestamo p JOIN p.cliente c WHERE c.id = :idCliente")
+    List<Prestamo> findByIdCliente(@Param("idCliente") Integer idCliente);
 
+    // Busqueda de prestamo por libro asociado
+    @Query("SELECT p FROM Prestamo p JOIN p.libro l WHERE l.isbn = :libroIsbn")
+    List<Prestamo> findByIsbn(@Param("libroIsbn") Integer libroIsbn);
 }
