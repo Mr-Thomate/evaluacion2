df0375489bab1d013a6618632b8c142b0d1c952a
mat-di <mata.diazs@duocuc.cl>
2026-05-10 22:23:46 -0400

    Update RegionRepository.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/RegionRepository.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/RegionRepository.java
@@ -1,17 +1,11 @@
 package evaluacion2.evaluacion2.repository;
 
 import org.springframework.stereotype.Repository;
-
 import evaluacion2.evaluacion2.model.Region;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-
-
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer>{
