da9973d89c0809a8c0e1587da3c8f648b72eb94d
mat-di <mata.diazs@duocuc.cl>
2026-05-10 22:21:08 -0400

    Update AutorRepository.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/AutorRepository.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/AutorRepository.java
@@ -1,11 +1,8 @@
 package evaluacion2.evaluacion2.repository;
 
 import evaluacion2.evaluacion2.model.Autor;
-
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
