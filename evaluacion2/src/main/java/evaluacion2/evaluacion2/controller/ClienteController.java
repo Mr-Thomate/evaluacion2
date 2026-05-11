61997fba3e5da4c468b7f54014e2a8514558eefc
mat-di <mata.diazs@duocuc.cl>
2026-05-10 22:13:28 -0400

    Update ClienteController.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/controller/ClienteController.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/controller/ClienteController.java
@@ -1,7 +1,6 @@
 package evaluacion2.evaluacion2.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import evaluacion2.evaluacion2.dto.ClienteDTO;
 import evaluacion2.evaluacion2.model.Cliente;
 import evaluacion2.evaluacion2.service.ClienteService;
