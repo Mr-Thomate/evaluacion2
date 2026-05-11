7cab29a8e35c66b28e3d45de0b0c6573a3efa5d1
mat-di <mata.diazs@duocuc.cl>
2026-05-10 22:08:01 -0400

    Update EmpleadoService.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/service/EmpleadoService.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/service/EmpleadoService.java
@@ -30,6 +30,17 @@ public class EmpleadoService {
         return empleadoRepository.save(nuevoEmpleado);
     }
 
+    public String eliminar(Integer id) {
+        try {
+            Empleado empleado = empleadoRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Error: No se encuentra el empleado"));
+            empleadoRepository.delete(empleado);
+            return "El empleado " + empleado.getId() + " ha sido eliminado con exito.";
+        } catch (RuntimeException e) {
+            return e.getMessage();
+        }
+    }
+
     public Empleado actualizar(Integer id, Empleado empleado) {
         Empleado emp = empleadoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Error: El empleado no existe."));
