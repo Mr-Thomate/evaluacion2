054feb880de96dbf82652c7c3a6d4bf8c704b8b0
mat-di <mata.diazs@duocuc.cl>
2026-05-10 22:22:01 -0400

    Update ClienteRepository.java

--- evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/ClienteRepository.java
+++ evaluacion2/src/main/java/evaluacion2/evaluacion2/repository/ClienteRepository.java
@@ -1,12 +1,10 @@
 package evaluacion2.evaluacion2.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import evaluacion2.evaluacion2.model.Cliente;
 
 @Repository
